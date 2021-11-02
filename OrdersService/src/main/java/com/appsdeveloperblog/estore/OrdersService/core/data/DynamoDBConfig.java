a6e0a8b65cb4f0f506efb5d889ef36118fccdecf
jackieung451 <84652073+jackieung451@users.noreply.github.com>
2021-11-02 12:47:16 -0700

    Create DynamoDBConfig.java

---
+++ OrdersService/src/main/java/com/appsdeveloperblog/estore/OrdersService/core/data/DynamoDBConfig.java
@@ -0,0 +1,28 @@
+package com.appsdeveloperblog.estore.OrdersService.core.data;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
+@Configuration
+public class DynamoDBConfig {
+
+	@Bean
+	public DynamoDBMapper dynamoDBMapper() {
+		return new DynamoDBMapper(buildAmazonDynamoDB());
+	}
+
+	private AmazonDynamoDB buildAmazonDynamoDB() {
+		return AmazonDynamoDBClientBuilder
+				.standard()
+				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("dynamodb.us-east-1.amazonaws.com", "us-east-1"))
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("your access key", "your secret key"))).build();
+	}
+
+}
